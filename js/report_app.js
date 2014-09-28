2ee6a9af0e77e160fa0b83215adb81e8c852db7f
kong19881230 <kong19881230@gmail.com>
2014-09-28 09:51:18 +0800

    support new maint report

---
+++ js/report_app.js
@@ -0,0 +1,54 @@
+function saveMReport(report){
+	var mReportQueue=[];
+
+	var mReportQueueStr=window.localStorage.getItem("mReportQueue");
+	if(mReportQueueStr!=null){
+		mReportQueue =JSON.parse(mReportQueueStr); 
+	}
+	if(report.index>=0){
+		
+		mReportQueue[report.index]=report;
+		
+	}else{
+		report.index=0;
+		mReportQueue.push(report);
+		if(mReportQueue.length>3){
+			mReportQueue.shift();
+		}
+
+	}
+	window.localStorage.setItem("mReportQueue", JSON.stringify(mReportQueue));
+	window.localStorage.setItem("currentMReport", JSON.stringify(report));	
+
+}
+
+function saveSReport(report){
+	if(report.state=="uploaded"){
+		report.state="modified";
+	}
+
+	var sReportQueue=[];
+
+	var sReportQueueStr=window.localStorage.getItem("sReportQueue");
+
+	if(sReportQueueStr!=null){
+		sReportQueue =JSON.parse(sReportQueueStr); 
+	}
+	if(report.index>=0){
+		
+		sReportQueue[report.index]=report;
+		
+	}else{
+
+		report.index=0;
+		sReportQueue.push(report);
+		if(sReportQueue.length>3){
+			sReportQueue.shift();
+		}
+
+	}
+	
+	window.localStorage.setItem("sReportQueue", JSON.stringify(sReportQueue));
+	window.localStorage.setItem("currentSReport", JSON.stringify(report));	
+
+}
