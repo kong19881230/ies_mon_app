f274dcde6fb7e0d1dd9e5cfe1139648801cd7dd7
thomas un <thomasun@thomasuns-MacBook-Pro.local>
2014-10-02 13:45:10 +0800

    change the layout to protiart

--- js/report_app.js
+++ js/report_app.js
@@ -1,4 +1,7 @@
 function saveMReport(report){
+	if(report.status=="uploaded"){
+		report.status="modified";
+	}
 	var mReportQueue=[];
 
 	var mReportQueueStr=window.localStorage.getItem("mReportQueue");
@@ -21,10 +24,32 @@ function saveMReport(report){
 	window.localStorage.setItem("currentMReport", JSON.stringify(report));	
 
 }
+function syncMReport(report){
+	var mReportQueue=[];
 
+	var mReportQueueStr=window.localStorage.getItem("mReportQueue");
+	if(mReportQueueStr!=null){
+		mReportQueue =JSON.parse(mReportQueueStr); 
+	}
+	if(report.index>=0){
+		
+		mReportQueue[report.index]=report;
+		
+	}else{
+		report.index=0;
+		mReportQueue.push(report);
+		if(mReportQueue.length>3){
+			mReportQueue.shift();
+		}
+
+	}
+	window.localStorage.setItem("mReportQueue", JSON.stringify(mReportQueue));
+	window.localStorage.setItem("currentMReport", JSON.stringify(report));	
+
+}
 function saveSReport(report){
-	if(report.state=="uploaded"){
-		report.state="modified";
+	if(report.status=="uploaded"){
+		report.status="modified";
 	}
 
 	var sReportQueue=[];
